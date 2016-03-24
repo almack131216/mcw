b23edb9c4d8fcce985bd3409def7812d30c4450f
al20031 <al_2003_1@hotmail.com>
2016-03-24 10:06:14 +0100

    remove _ref folder

--- _ref/160303/app/js/annotated/common.js
+++
@@ -1,6 +0,0 @@
-window.lazySizesConfig = window.lazySizesConfig || {};
-window.lazySizesConfig.customMedia = {
-    '--small': '(max-width: 480px)',
-    '--medium': '(max-width: 900px)',
-    '--large': '(max-width: 1400px)'
-};
